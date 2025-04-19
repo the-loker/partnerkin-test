200a103030f03e5abf91467bffc8ca1579f34144
Владислав Шевердин <vladislavseverdin@loker.local>
2025-04-19 16:46:18 +0300

    Update widget order-card

--- client/widgets/order-card/ui/OrderCard.vue
+++ client/widgets/order-card/ui/OrderCard.vue
@@ -3,15 +3,32 @@
 
   const props = defineProps(orderCardProps);
   const { order } = toRefs(props);
+
+  const dateFormat = computed(() =>
+    new Intl.DateTimeFormat('ru-RU', { day: 'numeric', month: 'long' }).format(
+      order.value.publish_date
+    )
+  );
 </script>
 
 <template>
   <article class="order-card">
-    <div>{{ order.name }}</div>
+    <div class="order-card__header">
+      <h3 class="order-card__header-title">{{ order.name }}</h3>
+
+      <div class="order-card__header-price">{{ order.price }} руб.</div>
+    </div>
+
+    <div class="order-card__info">
+      <div class="order-card__info-user">{{ order.user_login }}</div>
+
+      <div class="order-card__info-date">{{ dateFormat }}</div>
+    </div>
+
+    <div class="order-card__description">{{ order.text }}</div>
   </article>
 </template>
 
 <style lang="scss">
-  @use './style.scss';
-
+  @import './style.scss';
 </style>
