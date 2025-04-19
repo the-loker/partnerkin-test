4bf5d4d73664a466f3c3a20b5090bcf8677eb7de
Владислав Шевердин <vladislavseverdin@loker.local>
2025-04-19 15:40:35 +0300

    Update layout default

--- client/app/layouts/default.vue
+++ client/app/layouts/default.vue
@@ -4,8 +4,8 @@
 </script>
 
 <template>
-  <div class="wrapper">
-    <Sidebar>
+  <div class="wrapper layout-default">
+    <Sidebar class="layout-default__sidebar">
       <TopCategories />
     </Sidebar>
 
@@ -14,3 +14,40 @@
     </main>
   </div>
 </template>
+
+<style lang="scss">
+  .layout-default {
+    $layout-padding: 20px;
+    $sidebar-width: 235px;
+
+    display: grid;
+
+    width: 100%;
+    max-width: 1024px;
+    padding: $layout-padding;
+
+    background-color: #f3f5f8;
+
+    gap: 24px;
+    grid-template-columns: $sidebar-width 1fr;
+    grid-template-rows: calc(100vh - $layout-padding * 2);
+
+    @media (max-width: 1023px) {
+      $sidebar-width: 216px;
+
+      max-width: 768px;
+
+      grid-template-columns: $sidebar-width 1fr;
+    }
+
+    @media (max-width: 767px) {
+      max-width: 360px;
+    }
+
+    &__sidebar {
+      @media (max-width: 767px) {
+        display: none;
+      }
+    }
+  }
+</style>
