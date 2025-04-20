8b94ca316d5a8a51a596acf94cbc8a1c5c0acc41
Владислав Шевердин <vladislavseverdin@loker.local>
2025-04-20 10:32:43 +0300

    Update entity order api

--- client/entities/order/api/index.ts
+++ client/entities/order/api/index.ts
@@ -1,10 +1,12 @@
 import { useMainApi } from '@/shared/api';
+import { searchParamsToObject } from '@/shared/lib';
 
 import type { TMainApiResponse } from '@shared/api/types';
 import type { IOrder } from '@entities/order';
 
-export function getOrders(): Promise<Array<IOrder>> {
+export function getOrders(query?: URLSearchParams): Promise<Array<IOrder>> {
   return useMainApi<TMainApiResponse<IOrder>>('/list', {
     method: 'GET',
+    query: query ? searchParamsToObject(query) : undefined,
   }).then((res) => res.response.data.list);
 }
