7bd38c902f7bf7fdeee1a8abd190a289338c2d57
Владислав Шевердин <vladislavseverdin@loker.local>
2025-04-20 11:21:47 +0300

    Update layout default

--- client/app/layouts/default.vue
+++ client/app/layouts/default.vue
@@ -1,49 +1,66 @@
 <script lang="ts" setup>
   import { Sidebar } from '@/widgets/sidebar';
   import { TopCategories } from '@/widgets/top-categories';
+  import { Banner } from '@/widgets/banner';
 </script>
 
 <template>
   <div class="wrapper layout-default">
-    <Sidebar class="layout-default__sidebar">
-      <TopCategories />
-    </Sidebar>
+    <Banner width="100%" height="70px" />
 
-    <main>
-      <slot />
-    </main>
+    <div class="layout-default__inner">
+      <Sidebar class="layout-default__sidebar">
+        <TopCategories />
+
+        <Banner width="100%" height="312px" />
+      </Sidebar>
+
+      <main>
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
   .layout-default {
-    $layout-padding: 20px;
     $sidebar-width: 235px;
 
-    display: grid;
+    display: flex;
+    flex-direction: column;
 
     width: 100%;
     max-width: 1024px;
     min-height: 100dvh;
-    padding: $layout-padding;
+    padding: 20px;
 
-    background-color: #f3f5f8;
+    background-color: var(--bg-light);
 
-    gap: 24px;
-    grid-template-columns: $sidebar-width 1fr;
+    gap: 20px;
 
     @media (max-width: 1023px) {
-      $sidebar-width: 216px;
-
       max-width: 768px;
-
-      grid-template-columns: $sidebar-width 1fr;
     }
 
     @media (max-width: 767px) {
       max-width: 360px;
+    }
 
-      grid-template-columns: 100%;
+    &__inner {
+      display: grid;
+
+      gap: 24px;
+      grid-template-columns: $sidebar-width 1fr;
+
+      @media (max-width: 1023px) {
+        $sidebar-width: 216px;
+
+        grid-template-columns: $sidebar-width 1fr;
+      }
+
+      @media (max-width: 767px) {
+        grid-template-columns: 100%;
+      }
     }
 
     &__sidebar {
