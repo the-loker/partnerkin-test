7cd8c7ea9d2a2f6adf356b6b53ce3ae9cefb82be
Владислав Шевердин <vladislavseverdin@loker.local>
2025-04-20 10:26:19 +0300

    Create order entity store

--- client/entities/order/model/index.ts
+++ client/entities/order/model/index.ts
@@ -1,1 +1,27 @@
 export * from './types';
+
+import { getOrders } from '../api';
+import type { IOrder } from './types';
+
+export const useOrderStore = defineStore('orderStore', () => {
+  const orders = ref<Array<IOrder>>([]);
+
+  function loadOrders(query?: URLSearchParams): Promise<Array<IOrder>> {
+    return getOrders(query);
+  }
+
+  function setOrders(data: Array<IOrder>) {
+    orders.value = data;
+  }
+
+  function appendOrders(data: Array<IOrder>) {
+    orders.value.push(...data);
+  }
+
+  return {
+    orders,
+    loadOrders,
+    setOrders,
+    appendOrders,
+  };
+});
