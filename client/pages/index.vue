a11254f18686ceff3430afc4276f86d4196785d0
Владислав Шевердин <vladislavseverdin@loker.local>
2025-04-21 22:52:51 +0300

    Update index page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
   import { OrderList } from '@/widgets/order-list';
   import { OrderCard } from '@/widgets/order-card';
+  import { Banner } from '@/widgets/banner';
   import { useOrderStore } from '@/entities/order';
   import { useApiErrorHandler } from '@/shared/api';
   import { usePaginateScroll } from '@/shared/composables';
@@ -55,15 +56,121 @@
 </script>
 
 <template>
-  <OrderList :orders="orders">
-    <template v-slot="order">
-      <OrderCard :order="order" />
-    </template>
+  <div class="home-page">
+    <Banner
+      class="home-page-banner"
+      width="100%"
+      height="94px"
+      :closable="true">
+      <div class="home-page-banner__header">
+        <h3 class="home-page-banner__header-title">
+          Хочешь разметить заказ или услугу?
+        </h3>
 
-    <template #pagination>
-      <div ref="container">
-        <span v-if="loading">Загрузка...</span>
+        <p class="home-page-banner__header-subtitle">
+          Кликай и разместись за 1 минуту
+        </p>
       </div>
-    </template>
-  </OrderList>
+
+      <img
+        class="home-page-banner__image-arrow"
+        src="/images/banner-arrow.svg"
+        alt=""
+        loading="lazy" />
+
+      <img
+        class="home-page-banner__image-illustration"
+        src="/images/banner-illustration.svg"
+        alt=""
+        loading="lazy" />
+    </Banner>
+
+    <OrderList :orders="orders">
+      <template v-slot="order">
+        <OrderCard :order="order" />
+      </template>
+
+      <template #pagination>
+        <div ref="container">
+          <span v-if="loading">Загрузка...</span>
+        </div>
+      </template>
+    </OrderList>
+  </div>
 </template>
+
+<style lang="scss">
+  .home-page {
+    display: flex;
+    flex-direction: column;
+
+    gap: 16px;
+  }
+
+  .home-page-banner {
+    position: relative;
+
+    padding: 16px;
+
+    background-color: #fff6c7;
+
+    @media (max-width: 1023px) {
+      height: 114px !important;
+    }
+
+    @media (max-width: 767px) {
+      display: none;
+    }
+
+    &__header {
+      display: flex;
+      flex-direction: column;
+
+      gap: 6px;
+
+      @media (max-width: 1023px) {
+        width: 65%;
+      }
+
+      &-title {
+        color: var(--color-primary-black);
+
+        @include heading-medium-large;
+      }
+
+      &-subtitle {
+        margin: 0;
+
+        color: var(--color-primary-black);
+
+        @include text-small;
+      }
+    }
+
+    &__image-arrow,
+    &__image-illustration {
+      position: absolute;
+    }
+
+    &__image-arrow {
+      top: 46px;
+      right: 200px;
+
+      @media (max-width: 1023px) {
+        display: none;
+      }
+    }
+
+    &__image-illustration {
+      top: 0;
+      right: 40px;
+
+      @media (max-width: 1023px) {
+        top: 26px;
+
+        width: 138px;
+        height: 82px;
+      }
+    }
+  }
+</style>
